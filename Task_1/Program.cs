1f89feef3d2ef8926e979a73473c105a0b5c43e4
Artem <letnev2012@gmail.com>
2022-06-27 17:42:52 +0300

    Add task 1

---
+++ Task_1/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// Console.WriteLine("Enter three-digit number: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// //  int remains = number % 100;
+// //   Console.WriteLine($"The second digit: {remains / 10}");
+
+// int remains = number % 10;
+// Console.WriteLine(remains);
+// int q = number / 10;
+// Console.WriteLine(q);
+Console.WriteLine("Enter number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int threedigit = number % 1000;
+        Console.WriteLine($"The third number: {threedigit}");
+
