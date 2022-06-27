4b3e031ec2613b0d0f12f53e506017c44411dec3
Artem <letnev2012@gmail.com>
2022-06-27 17:43:09 +0300

    Add task 3

---
+++ Task_3/Program.cs
@@ -0,0 +1,47 @@
+﻿//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.WriteLine("Enter number 1-7");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 0 && number < 8)
+{
+    if (number == 1)
+    {
+        Console.WriteLine("Monday");
+        Console.WriteLine("The day is not a weekend");
+    }
+    if (number == 2)
+    {
+        Console.WriteLine("Tuesday");
+        Console.WriteLine("The day is not a weekend");
+    }
+    if (number == 3)
+    {
+        Console.WriteLine("Wednesday");
+        Console.WriteLine("The day is not a weekend");
+    }
+    if (number == 4)
+    {
+        Console.WriteLine("Thursday");
+        Console.WriteLine("The day is not a weekend");
+    }
+    if (number == 5)
+    {
+        Console.WriteLine(" Friday");
+        Console.WriteLine("The day is not a weekend");
+    }
+    if (number == 6)
+    {
+        Console.WriteLine("Saturday");
+        Console.WriteLine("The day is a weekend");
+    }
+    if (number == 7)
+    {
+        Console.WriteLine("Sunday");
+        Console.WriteLine("The day is a weekend");
+    }
+}
+else 
+{
+        Console.WriteLine("Enter only 1-7 numbers");
+}
